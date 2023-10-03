663c314820c1d445ef80d009391d8923d000532f
henriquehiga <codehiga@gmail.com>
2023-10-02 21:19:39 -0300

    feat: airesponse entity

--- src/core/domain/entity/ai-response.ts
+++ src/core/domain/entity/ai-response.ts
@@ -1,10 +1,17 @@
-export type AiResponseOptions = {
-
+export type AiResponseProps = {
+    role: string;
+    content: string;
 }
 
 export class AiResponse {
     readonly role: string;
     readonly content: string;
 
-    private constructor() { }
+    private constructor(props: AiResponseProps) {
+        Object.assign(this, props);
+    }
+
+    static create(props: AiResponseProps) {
+        return new AiResponse(props);
+    }
 }
